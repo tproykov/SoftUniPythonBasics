66027983d68064f2a220f26cee3c0a02faad4efa
tproykov <tproykov@gmail.com>
2025-09-07 11:31:14 +0100

    Create E00Tests.py

---
+++ PB01FirstSteps/E00Tests.py
@@ -0,0 +1,17 @@
+print('What is your first name?')
+first_name = input()
+print('What is your last name?')
+last_name = input()
+print('What is your age?')
+age = input()
+print(f'Hello, {first_name} {last_name}! You are {age} years old!')
+
+# age_as_str = '20'
+# print(age)
+# print(type(age))
+# # print(age_as_str)
+# # print(type(age_as_str))
+# # print(age + age_as_str)
+# # print(age - age_as_str)
+
+
